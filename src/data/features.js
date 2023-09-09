fed80918dd04364e0ae03831c39727a216153ed7
Fredericks <adefredy1@gmail.com>
2023-09-09 11:31:08 +0100

    Create the features array in data folder, export from the index.js in same (for easy import)

---
+++ src/data/features.js
@@ -0,0 +1,64 @@
+import { estate_frontal, estate_lawn, estate_lit, estate_sky } from "@/assets/images";
+
+export const features = [
+    {
+        id: 89230,
+        title: "Family Apartment",
+        address: "City Gate",
+        city: "Abuja",
+        country: "Nigeria",
+        type: "apartment",
+        image: estate_lit,
+        price: 5000000,
+        extras: {
+            beds: 4,
+            baths: 3,
+            sqft: 210,
+        },
+    },
+    {
+        id: 89232,
+        title: "Fancy Apartment",
+        address: "Lekki",
+        city: "Lagos",
+        country: "Nigeria",
+        type: "apartment",
+        image: estate_sky,
+        price: 5200000,
+        extras: {
+            beds: 4,
+            baths: 4,
+            sqft: 185,
+        },
+    },
+    {
+        id: 89231,
+        title: "Duplex Apartment",
+        address: "Katampe Extension",
+        city: "Abuja",
+        country: "Nigeria",
+        type: "apartment",
+        image: estate_lawn,
+        price: 3000000,
+        extras: {
+            beds: 5,
+            baths: 4,
+            sqft: 162,
+        },
+    },
+    {
+        id: 89232,
+        title: "Excellent Pool Apartment",
+        address: "New Shimawa",
+        city: "Lagos",
+        country: "Nigeria",
+        type: "apartment",
+        image: estate_frontal,
+        price: 2600000,
+        extras: {
+            beds: 3,
+            baths: 3,
+            sqft: 110,
+        },
+    },
+]
---
+++ src/data/index.js
@@ -0,0 +1,5 @@
+import {features} from "@/data/features"
+
+export {
+    features,
+}

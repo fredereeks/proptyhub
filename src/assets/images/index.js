60940f5c75f719d50bac9d6cd588cd266d8a5287
Fredericks <adefredy1@gmail.com>
2023-09-09 11:29:03 +0100

    Copy all images to the assets/images folder and export in the index.js for easy import

---
+++ src/assets/images/index.js
@@ -0,0 +1,42 @@
+
+import estate_lit from "@/assets/images/estate_lit.jpg" 
+import estate_living_room from "@/assets/images/estate_living_room.jpg" 
+import estate_officespace from "@/assets/images/estate_officespace.jpg" 
+import estate_reading_table from "@/assets/images/estate_reading_table.jpg" 
+import estate_sky from "@/assets/images/estate_sky.jpg" 
+import estate_bathroom from "@/assets/images/estate_bathroom.jpg" 
+import estate_carton from "@/assets/images/estate_carton.jpg" 
+import estate_couple_carton from "@/assets/images/estate_couple_carton.jpg" 
+import estate_dining from "@/assets/images/estate_dining.jpg" 
+import estate_eating_table from "@/assets/images/estate_eating_table.jpg" 
+import estate_frontal from "@/assets/images/estate_frontal.jpg" 
+import estate_home1 from "@/assets/images/estate_home1.jpg" 
+import estate_home2 from "@/assets/images/estate_home2.jpg" 
+import estate_home3 from "@/assets/images/estate_home3.jpg" 
+import estate_home4 from "@/assets/images/estate_home4.jpg" 
+import estate_home5 from "@/assets/images/estate_home5.jpg" 
+import estate_home6 from "@/assets/images/estate_home6.jpg" 
+import estate_keys from "@/assets/images/estate_keys.jpg" 
+import estate_lawn from "@/assets/images/estate_lawn.jpg"
+
+export {
+    estate_lit,
+    estate_living_room,
+    estate_officespace,
+    estate_reading_table,
+    estate_sky,
+    estate_bathroom,
+    estate_carton,
+    estate_couple_carton,
+    estate_dining,
+    estate_eating_table,
+    estate_frontal,
+    estate_home1,
+    estate_home2,
+    estate_home3,
+    estate_home4,
+    estate_home5,
+    estate_home6,
+    estate_keys,
+    estate_lawn,
+}

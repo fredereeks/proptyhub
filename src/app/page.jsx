d57db67e230bae7b339989abd029cad7efcc1eeb
Fredericks <adefredy1@gmail.com>
2023-09-09 11:26:56 +0100

    Commence creation of a Real Estate Website

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+    return (
+        <main className="relative">
+            <p className="text-slate-700">Home Page</p>
+        </main>
+    )
+}
